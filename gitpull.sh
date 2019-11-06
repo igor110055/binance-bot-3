461bafe446ce0d34331c8b563bd75641543adf8d
jmit-modern <jmit.agency@gmail.com>
2019-11-06 13:21:45 +0800

    .

--- gitpull.sh
+++ gitpull.sh
@@ -3,7 +3,7 @@ echo ‘post-receive: Triggered.’
 current=$(basename "$(pwd)")
 echo "${current}"
 cd "$(dirname "$0")"
-git pull \
-&& echo "Forever restarting.."
-&& forever restart "${current}"
-&& echo "Forever restarted."
+git pull \ 
+echo "Forever restarting.."\
+forever restart "${current}"\
+echo "Forever restarted."\
