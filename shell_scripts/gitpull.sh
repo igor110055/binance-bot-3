687b9eae58cdfcda9caac7450bf77d421b5609be
jmit-modern <jmit.agency@gmail.com>
2019-11-07 23:24:06 +0800

    .

--- shell_scripts/gitpull.sh
+++ shell_scripts/gitpull.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo ‘post-receive: Triggered.’
 current=$(basename "$(pwd)")
-echo "$(pwd "$1")"
+echo "$(basename $(pwd "$1"))"
 echo "${current}"
 cd "$(dirname "$0")"
 git reset --hard
