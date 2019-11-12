6a622aeb9a377890d8343bf85664028d98edb277
jmit-modern <jmit.agency@gmail.com>
2019-11-13 00:51:46 +0800

    .

--- binbot.js
+++ binbot.js
@@ -174,6 +174,7 @@ function market_Sell(symbol, symbolPrice){
 
 setTimeout(() => {
     subscribe();
+    exchangeInfo();
 }, 2000);
 
 function subscribe(){
@@ -406,32 +407,34 @@ function finalStep(){
 * minQty = minimum order quantity
 * minNotional = minimum order value (price * quantity) 
 */
-binance.exchangeInfo(function(error, data) {
-    let minimums = {};
-    for ( let obj of data.symbols ) {
-        if(!usePairs.includes(obj.symbol)) continue;
-        let filters = {status: obj.status};
-        for ( let filter of obj.filters ) {
-            if ( filter.filterType == "MIN_NOTIONAL" ) {
-                filters.minNotional = filter.minNotional;
-            } else if ( filter.filterType == "PRICE_FILTER" ) {
-                filters.minPrice = filter.minPrice;
-                filters.maxPrice = filter.maxPrice;
-                filters.tickSize = filter.tickSize;
-            } else if ( filter.filterType == "LOT_SIZE" ) {
-                filters.stepSize = filter.stepSize;
-                filters.minQty = filter.minQty;
-                filters.maxQty = filter.maxQty;
+function exchangeInfo(){
+    binance.exchangeInfo(function(error, data) {
+        let minimums = {};
+        for ( let obj of data.symbols ) {
+            if(!usePairs.includes(obj.symbol)) continue;
+            let filters = {status: obj.status};
+            for ( let filter of obj.filters ) {
+                if ( filter.filterType == "MIN_NOTIONAL" ) {
+                    filters.minNotional = filter.minNotional;
+                } else if ( filter.filterType == "PRICE_FILTER" ) {
+                    filters.minPrice = filter.minPrice;
+                    filters.maxPrice = filter.maxPrice;
+                    filters.tickSize = filter.tickSize;
+                } else if ( filter.filterType == "LOT_SIZE" ) {
+                    filters.stepSize = filter.stepSize;
+                    filters.minQty = filter.minQty;
+                    filters.maxQty = filter.maxQty;
+                }
             }
+            filters.orderTypes = obj.orderTypes;
+            filters.icebergAllowed = obj.icebergAllowed;
+            minimums[obj.symbol] = filters;
         }
-        filters.orderTypes = obj.orderTypes;
-        filters.icebergAllowed = obj.icebergAllowed;
-        minimums[obj.symbol] = filters;
-    }
-    global.filters = minimums;
-    // console.log(global.filters);
-    //fs.writeFile("minimums.json", JSON.stringify(minimums, null, 4), function(err){});
-});
+        global.filters = minimums;
+        // console.log(global.filters);
+        //fs.writeFile("minimums.json", JSON.stringify(minimums, null, 4), function(err){});
+    });
+}
 
 /**
  * Cut off decimals to efficient number after dot
